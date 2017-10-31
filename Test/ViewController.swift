e0a44d9b637af91169d147d937522037cf607b63
Peter Barnett <pfbarnet@gmail.com>
2017-10-31 14:25:53 -0700

    my first commit

--- Test/ViewController.swift
+++ Test/ViewController.swift
@@ -9,12 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+   
+    @IBOutlet weak var HelloWorldl: UILabel!
+    
+    var tapCount = 0
+    
+    
+    
+    @IBAction func buttonTapped(_ sender: Any) {
+    
+    HelloWorldl.text = "Hello there!"
+       print("tapped")
+        tapCount = tapCount + 1
+        if tapCount >= 10 {
+            HelloWorldl.text = "You tapped 10 times!"
+        }
+    }
+    
+    @IBAction func what(_ sender: Any) {
+        
+      HelloWorldl.text = "Oh hi"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
